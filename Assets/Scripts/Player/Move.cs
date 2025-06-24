57529dbb18439b1c01c2feb39587cd872e410d84
Huu Hoai Linh Nguyen <hoailinh.dienban@gmail.com>
2025-06-24 15:12:00 +0700

    [LinhNHH] Fix enemy

--- Assets/Scripts/Player/Move.cs
+++ Assets/Scripts/Player/Move.cs
@@ -11,22 +11,17 @@ public class PlayerMovement : MonoBehaviour
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-
-        
+        rb = GetComponent<Rigidbody2D>();               
         rb.freezeRotation = true;
     }
 
     void Update()
-    {
-        
+    {        
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-
-        
+                
         moveInput = new Vector2(moveX, moveY).normalized;
-
-      
+              
         if (moveX > 0)
             transform.localScale = new Vector3(-1, 1, 1); 
         else if (moveX < 0)
@@ -34,8 +29,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void FixedUpdate()
-    {
-        
+    {        
         rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
     }
 }
--- Assets/Scripts/UI/MainMenu/VideoEndTrigger.cs
+++ Assets/Scripts/UI/MainMenu/VideoEndTrigger.cs
@@ -4,25 +4,22 @@ using UnityEngine.SceneManagement; // Để sử dụng SceneManager
 
 public class VideoEndTrigger : MonoBehaviour
 {
-    public VideoPlayer videoPlayer;  // Tham chiếu đến VideoPlayer
-    public string sceneToLoad = "Scenes/MainMenu";  // Tên scene bạn muốn chuyển đến khi video kết thúc
+    public VideoPlayer videoPlayer;
+    public string sceneToLoad = "Scenes/MainMenu";
 
     void Start()
     {
-        // Đảm bảo VideoPlayer đã được gán
         if (videoPlayer == null)
         {
             videoPlayer = GetComponent<VideoPlayer>();
         }
 
-        // Đăng ký sự kiện khi video kết thúc
         videoPlayer.loopPointReached += OnVideoEnd;
     }
 
-    // Hàm được gọi khi video kết thúc
     void OnVideoEnd(VideoPlayer vp)
     {
         Debug.Log("Video has finished. Loading next scene...");
-        SceneManager.LoadScene(sceneToLoad);  // Chuyển đến scene mới
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
